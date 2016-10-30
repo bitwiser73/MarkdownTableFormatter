674c9413400f4b01828010bfb49ea38096d8202a
bitwiser <markdowntableformatter@gmail.com>
2016-10-30 16:22:16 +0100

    Enhance unit test

--- tests/test.py
+++ tests/test.py
@@ -8,35 +8,51 @@ sys.path.append(os.path.realpath('..'))
 import simple_markdown.table
 import simple_markdown.table as Table
 
-#import unittest
+import unittest
 
-raw_table = """\
+class test_markdown_table(unittest.TestCase):
+    def setUp(self):
+        pass
+    def tearDown(self):
+        pass
+
+    def test_format(self):
+        raw_table = """\
 |   Tables        | Are       | Cool  |
-|-------------|:-------------|:-----:|
- | col 1 is      | left-aligned | $1600 |  
-col 2 is      ||   $12  
-  | zebra stripes | are neat      |    $1 |  
+|:-------------|-------------|-----:|
+ | col 1 is      | left-aligned                                    | $1600 |  
+col 2 is      | centered|   $12  
+  | zebra stripes |       | are neat   $1 |  
 || |$hello
 |    $2 |"""
+    
+        expected_table_2_2 = """\
+|  Tables           |       Are        |             Cool  |
+|:------------------|:----------------:|------------------:|
+|  col 1 is         |   left-aligned   |            $1600  |
+|  col 2 is         |     centered     |              $12  |
+|  zebra stripes    |                  |    are neat   $1  |
+|                   |                  |           $hello  |
+|  $2               |                  |                   |"""
 
-expected_table = """\
-| Tables        | Are          | Cool   |
-|:--------------|:-------------|:------:|
-| col 1 is      | left-aligned | $1600  |
-| col 2 is      |              | $12    |
-| zebra stripes | are neat     | $1     |
-|               |              | $hello |
-| $2            |              |        |"""
+        table = Table.format(raw_table, margin=2, padding=2,
+                             default_justify=Table.Justify.CENTER)
+        self.assertEqual(table, expected_table_2_2)
 
-print(raw_table)
-table = Table.format(raw_table, margin=1, padding=0,
-                     default_justify=Table.Justify.CENTER)
-print(table)
+        expected_table_0_0 = """\
+|Tables       |Are         |         Cool|
+|:------------|:-----------|------------:|
+|col 1 is     |left-aligned|        $1600|
+|col 2 is     |centered    |          $12|
+|zebra stripes|            |are neat   $1|
+|             |            |       $hello|
+|$2           |            |             |"""
 
-if table == expected_table:
-    print("OK")
+        table = Table.format(raw_table, margin=0, padding=0)
+        self.assertEqual(table, expected_table_0_0)
 
-junk_tables = """
+    def test_find_all(self):
+        junk_tables = """
 |   Tables        | Are       | Cool #1  |
 |-------------|:-------------:|:-----|
  | col 3 is      | right-aligned | $1600 |  
@@ -57,13 +73,17 @@ col 2 is      ||   $12
 || |$hello
 |    $2 |
 
+| ok | more | col | and more | col
+|----|-----|--|----|---|--|-|-|-|
+|ok | still good
+
 junk junk junk
-and some | to test
+and some | to test |
 if it's still working ||||||
 is it?
 """
+        offsets = simple_markdown.table.find_all(junk_tables)
+        self.assertEqual(len(offsets), 3)
 
-offsets = simple_markdown.table.find_all(junk_tables)
-for offset in offsets:
-    table = simple_markdown.table.format(junk_tables[offset[0]:offset[1]])
-    print(table + "\n")
+if __name__ == '__main__':
+    unittest.main()
